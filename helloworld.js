466626e5cf73b3a46e049e3c516e7edb48945e6e
Chiloplacus <c.r.gower@gmail.com>
2025-01-31 19:17:26 -0600

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Welcome to my AWS Web App!\n')
+   response.end('Hello, Comapny MU! The web server has been updated via Ansible. \n')
 }).listen(3333)
 
 // Console will print the message
